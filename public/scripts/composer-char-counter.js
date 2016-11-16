781a0ae31553c1a7a4ef7fd0cdae6241c869856e
Ghislain H <telitos@gmail.com>
2016-11-16 00:55:12 +0000

    fixes counter issues

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,17 +4,35 @@
 $(function() {
 
   $("main.container textarea").keydown(function() {
-    const start = 140
-    let charNumber = $(this).val().length
-    let counter = start - charNumber
-    //Find a way to store the element $(this).siblings('/coutner')
-    //into a variable. Why did it not work?
-    $(this).siblings('.counter').text(counter)
 
-    if ($(this).siblings('.counter').text() < 1) {
-      $(this).siblings('.counter').css("color","red")
-    } else {
-      $(this).siblings('.counter').css("color","blue")
-    }
+console.log('sup')
+
+    let start = 140
+    //the timeout here makes sure that the first iteration
+    // of the loop only occurs once the dom has been updated
+
+    setTimeout(() => {
+      let charNumber = $(this).val().length
+      console.log("length:", charNumber)
+      let counter = start - charNumber
+      //Find a way to store the element $(this).siblings('/coutner')
+      //into a variable. Why did it not work?
+      $(this).siblings('.counter').text(counter)
+
+      if (counter < 1) {
+        $(this).siblings('.counter').addClass('over')
+      } else {
+        $(this).siblings('.counter').removeClass('over')
+      }
+    })
   })
+
+
+
+    //   ($(this).siblings('.counter').text() < 1) {
+    //   $(this).siblings('.counter').css("color","red")
+    // } else {
+    //   $(this).siblings('.counter').css("color","blue")
+    // }
+  // }
 })
